a404b2a36eb39ed81332214514d71045727141c1
Jakub Zima <jakubzima@student.agh.edu.pl>
2026-05-24 16:37:57 +0200

    loading another data "Titanic dataset"

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,15 +1,44 @@
 import pandas as pd
+from pathlib import Path
 
 from sklearn.datasets import load_breast_cancer
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+
+
+def load_breast_cancer_dataset():
+    data = load_breast_cancer()
+
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = pd.Series(data.target, name="target")
+
+    return X, y
+
+
+def load_titanic_dataset():
+    file_path = DATA_DIR / "titanic_agh.csv"
+    df = pd.read_csv(file_path)
+
+    df = df.drop(columns=["ID_pasażera"])
+
+    df["Płeć"] = df["Płeć"].map({"male": 0, "female": 1})
+    df["Port_zaokrętowania"] = df["Port_zaokrętowania"].map({"S": 0, "C": 1, "Q": 2})
+
+    df = df.dropna(subset=["Port_zaokrętowania"])
+
+    X = df.drop(columns=["Przeżył"]).copy()
+    y = df["Przeżył"].copy()
+
+    return X, y
+
+
 def load_dataset(dataset_name):
     if dataset_name == "breast_cancer":
-        data = load_breast_cancer()
+        return load_breast_cancer_dataset()
 
-        X = pd.DataFrame(data.data, columns=data.feature_names)
-        y = pd.Series(data.target, name="target")
-
-        return X, y
+    if dataset_name == "titanic":
+        return load_titanic_dataset()
 
     raise ValueError(f"Unsupported dataset name: {dataset_name}")
